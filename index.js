3eed38c297b2e27951a141dd024d7f7d7d7621f9
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-06-05 03:01:30 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 const server = new ApolloServer({
   typeDefs,
   resolvers,
